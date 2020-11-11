a503365f266ef02c0d39712518320f3fed4ea2c6
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2020-11-11 14:18:56 +0900

    Create sounds-barrage.js

---
+++ sounds-barrage.js
@@ -0,0 +1,39 @@
+/********************************************************************************
+
+	SYNCER 〜 知識、感動をみんなと同期(Sync)するブログ
+
+	* 配布場所
+	https://syncer.jp/html5-javascript-hello-button
+
+	* 最終更新日時
+	2015/08/15 03:50
+
+	* 作者
+	あらゆ
+
+	** 連絡先
+	Twitter: https://twitter.com/arayutw
+	Facebook: https://www.facebook.com/arayutw
+	Google+: https://plus.google.com/114918692417332410369/
+	E-mail: info@syncer.jp
+
+	※ バグ、不具合の報告、提案、ご要望など、お待ちしております。
+	※ 申し訳ありませんが、ご利用者様、個々の環境における問題はサポートしていません。
+
+********************************************************************************/
+
+
+function sound()
+{
+	// 対象となるID名
+	var id = 'sound-file' ;
+
+	// 初回以外だったら音声ファイルを巻き戻す
+	if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+	{
+		document.getElementById( id ).currentTime = 0;
+	}
+
+	// [ID:sound-file]の音声ファイルを再生[play()]する
+	document.getElementById( id ).play() ;
+}
