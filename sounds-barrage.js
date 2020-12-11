eb2aec5c1790ea74e5d2b507b1694a9c012c35b4
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2020-12-11 19:07:22 +0900

    Update sounds-barrage.js

--- sounds-barrage.js
+++ sounds-barrage.js
@@ -37,22 +37,3 @@ function sound()
 	// [ID:sound-file]の音声ファイルを再生[play()]する
 	document.getElementById( id ).play() ;
 }
-let song;
-
-function setup() {
-    song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
-    createCanvas(720, 200);
-    background(255, 0, 0);
-}
-
-function mousePressed() {
-    if (song.isPlaying()) {
-        // .isPlaying()はブール値を返す
-        song.stop();
-        background(255, 0, 0);
-    }
-    else {
-        song.play();
-        background(0, 255, 0);
-    }
-}
