73fed23f55b5227ea845a41c8ed9231ff9095725
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2020-12-21 20:10:27 +0900

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+let song;
+
+function preload() {
+    // サウンド形式に.oggファイルと.mp3ファイル両方を含める
+    soundFormats('ogg', 'mp3');
+
+    // このブラウザがmp3をサポートしない場合、
+    // loadSound()は、スケッチに含めておいたoggファイルを読み込む
+    song = loadSound('greeting-01.mp3');
+}
+
+function setup() {
+    createCanvas(710, 200);
+    background(0, 255, 0);
+
+    // ボタン
+    const button = setButton('PLAY', {
+        x: 40,
+        y: 180
+    });
+
+    button.mousePressed(() => {
+        // 再生中なら
+        if (song.isPlaying()) {
+            // 一時停止
+            song.pause();
+            // ボタンのラベルを変更
+            button.html('PLAY');
+            // 背景色を変更
+            background(255, 0, 0);
+            // 再生中でないなら
+        }
+        else {
+            // 再生
+            song.play();
+            button.html('PAUSE');
+            background(0, 255, 0);
+        }
+    });
+}
+
+function mousePressed() {
+    // サンプルから変更
+}
+
+function setButton(label, pos) {
+    const button = createButton(label);
+    button.size(100, 30);
+    button.position(pos.x, pos.y);
+    return button;
+}
