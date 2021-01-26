7136d9e9ed45f6a1eca20ecd9998efd8e1b1d72a
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2021-01-26 20:59:05 +0900

    Update index.js

--- sample/index.js
+++ sample/index.js
@@ -1,6 +1,32 @@
-html,body{
-    overflow: hidden;
+// スクロールを無効化
+        $('html').css({
+            overflow: 'hidden'
+        });
+        document.addEventListener(
+            'touchmove',
+            scrollControll,
+            {
+                passive: false
+            }
+        );
 
-$(window).on('touchmove.noScroll', function(e) {
-    e.preventDefault();
-});
+        // スクロールを有効化
+        $('html').css({
+            overflow: ''
+        });
+        document.removeEventListener(
+            'touchmove',
+            scrollControll,
+            {
+                passive: false
+            }
+        );
+
+    // add/removeEventListenerでアロー関数として定義するとイベントが削除されない
+    var scrollControll = function(event) {
+        if ($(event.target).closest('.js-can-scroll').length > 0) {
+            event.stopPropagation();
+        } else {
+            event.preventDefault();
+        }
+    };
