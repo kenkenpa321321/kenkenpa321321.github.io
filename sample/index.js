35e3569a52126052dd341e4b2f0be33c10e99923
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2021-01-26 20:40:50 +0900

    Update index.js

--- sample/index.js
+++ sample/index.js
@@ -5,13 +5,6 @@ function no_scroll() {
     // スマホでのタッチ操作でのスクロール禁止
     document.addEventListener("touchmove", scroll_control, { passive: false });
 }
-// スクロール禁止解除
-function return_scroll() {
-    // PCでのスクロール禁止解除
-    document.removeEventListener("mousewheel", scroll_control, { passive: false });
-    // スマホでのタッチ操作でのスクロール禁止解除
-    document.removeEventListener('touchmove', scroll_control, { passive: false });
-}
 
 // スクロール関連メソッド
 function scroll_control(event) {
