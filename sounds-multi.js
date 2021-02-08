0237e12f9a5cf504415ec68cad8bd8ca669e686f
kenkenpa321321.github.io <74258461+kenkenpa321321@users.noreply.github.com>
2021-02-08 23:16:18 +0900

    Update sounds-multi.js

--- sounds-multi.js
+++ sounds-multi.js
@@ -77,6 +77,221 @@ var syncerSounds = {
 		}
 	}
 
+
+		// 設定
+		var setClass = 'sounds2'   ;							// ボタン要素のクラス名
+		var setDir = './' ;									// 音声ファイルがあるフォルダ(最後は[/])
+		var setStopButtonId = 'stop-button-syncer' ;			// 停止ボタンに付けるID
+	
+		// クラス名が付いた要素を取得する
+		var sounds = document.getElementsByClassName( setClass ) ;
+	
+		// 全ての要素にクリックイベントを設定する
+		for( var i=0,l=sounds.length ; l>i ; i++ )
+		{
+			// クリックイベントの設定
+			sounds[i].onclick = function()
+			{
+				// ファイル名の取得
+				var file = this.getAttribute( 'data-file' ) ;
+	
+				// 一度生成したエレメントは生成しない
+				if( typeof( syncerSounds.flag[ file ] )=="undefined" || syncerSounds.flag[ file ] != 1 )
+				{
+					// 生成するエレメントの調整
+					var audio = document.createElement( 'audio' ) ;
+	
+					// エレメントのIDを指定
+					audio.id = file ;
+	
+					
+	
+					// ブラウザが[.wav]に対応している場合は[.wav]を読み込む
+					if( audio.canPlayType( 'audio/wav' ) )
+					{
+						audio.src = setDir + file + '.wav' ;
+					}
+	
+					// [audio]を生成する
+					document.body.appendChild( audio ) ;
+				}
+	
+				// 初回再生以外だったら音声ファイルを巻き戻す
+				stopCurrentSound() ;
+	
+				// 音声ファイルを再生[play()]する
+				document.getElementById( file ).play() ;
+	
+				if(file ==="kan"){document.area1.src = "images/ue.jpg"}
+				if(file ==="ice"){document.area1.src = "images/sita.jpg"}
+				if(file ==="soda"){document.area1.src = "images/migi.jpg"}
+				if(file ==="wave"){document.area1.src = "images/ue.jpg"}
+				if(file ==="fastener"){document.area1.src = "images/sita.jpg"}
+				if(file ==="pet"){document.area1.src = "images/hidari.jpg"}
+				if(file ==="spray"){document.area1.src = "images/ue.jpg"}
+				if(file ==="tomato"){document.area1.src = "images/sita.jpg"}
+				if(file ==="rain"){document.area1.src = "images/migi.jpg"}
+				if(file ==="huurin"){document.area1.src = "images/migi.jpg"}
+				if(file ==="suiteki"){document.area1.src = "images/ue.jpg"}
+				if(file ==="iki"){document.area1.src = "images/migi.jpg"}
+				if(file ==="potato"){document.area1.src = "images/sita.jpg"}
+				if(file ==="egg"){document.area1.src = "images/hidari.jpg"}
+				if(file ==="patipati"){document.area1.src = "images/migi.jpg"}
+				if(file ==="men"){document.area1.src = "images/hidari.jpg"}
+			
+				// 最近再生した音声としてセット
+				syncerSounds.currentTime = file ;
+	
+				
+				// 終了
+				return false ;
+			}
+		}
+
+
+
+			// 設定
+	var setClass = 'sounds3'   ;							// ボタン要素のクラス名
+	var setDir = './' ;									// 音声ファイルがあるフォルダ(最後は[/])
+	var setStopButtonId = 'stop-button-syncer' ;			// 停止ボタンに付けるID
+
+	// クラス名が付いた要素を取得する
+	var sounds = document.getElementsByClassName( setClass ) ;
+
+	// 全ての要素にクリックイベントを設定する
+	for( var i=0,l=sounds.length ; l>i ; i++ )
+	{
+		// クリックイベントの設定
+		sounds[i].onclick = function()
+		{
+			// ファイル名の取得
+			var file = this.getAttribute( 'data-file' ) ;
+
+			// 一度生成したエレメントは生成しない
+			if( typeof( syncerSounds.flag[ file ] )=="undefined" || syncerSounds.flag[ file ] != 1 )
+			{
+				// 生成するエレメントの調整
+				var audio = document.createElement( 'audio' ) ;
+
+				// エレメントのIDを指定
+				audio.id = file ;
+
+				
+
+				// ブラウザが[.wav]に対応している場合は[.wav]を読み込む
+				if( audio.canPlayType( 'audio/wav' ) )
+				{
+					audio.src = setDir + file + '.wav' ;
+				}
+
+				// [audio]を生成する
+				document.body.appendChild( audio ) ;
+			}
+
+			// 初回再生以外だったら音声ファイルを巻き戻す
+			stopCurrentSound() ;
+
+			// 音声ファイルを再生[play()]する
+			document.getElementById( file ).play() ;
+
+			if(file ==="kan"){document.area1.src = "images/ue.jpg"}
+			if(file ==="ice"){document.area1.src = "images/sita.jpg"}
+			if(file ==="soda"){document.area1.src = "images/migi.jpg"}
+			if(file ==="wave"){document.area1.src = "images/ue.jpg"}
+			if(file ==="fastener"){document.area1.src = "images/sita.jpg"}
+			if(file ==="pet"){document.area1.src = "images/hidari.jpg"}
+			if(file ==="spray"){document.area1.src = "images/ue.jpg"}
+			if(file ==="tomato"){document.area1.src = "images/sita.jpg"}
+			if(file ==="rain"){document.area1.src = "images/migi.jpg"}
+			if(file ==="huurin"){document.area1.src = "images/migi.jpg"}
+			if(file ==="suiteki"){document.area1.src = "images/ue.jpg"}
+			if(file ==="iki"){document.area1.src = "images/migi.jpg"}
+			if(file ==="potato"){document.area1.src = "images/sita.jpg"}
+			if(file ==="egg"){document.area1.src = "images/hidari.jpg"}
+			if(file ==="patipati"){document.area1.src = "images/migi.jpg"}
+			if(file ==="men"){document.area1.src = "images/hidari.jpg"}
+		
+			// 最近再生した音声としてセット
+			syncerSounds.currentTime = file ;
+
+			
+			// 終了
+			return false ;
+		}
+	}
+
+
+
+		// 設定
+		var setClass = 'sounds4'   ;							// ボタン要素のクラス名
+		var setDir = './' ;									// 音声ファイルがあるフォルダ(最後は[/])
+		var setStopButtonId = 'stop-button-syncer' ;			// 停止ボタンに付けるID
+	
+		// クラス名が付いた要素を取得する
+		var sounds = document.getElementsByClassName( setClass ) ;
+	
+		// 全ての要素にクリックイベントを設定する
+		for( var i=0,l=sounds.length ; l>i ; i++ )
+		{
+			// クリックイベントの設定
+			sounds[i].onclick = function()
+			{
+				// ファイル名の取得
+				var file = this.getAttribute( 'data-file' ) ;
+	
+				// 一度生成したエレメントは生成しない
+				if( typeof( syncerSounds.flag[ file ] )=="undefined" || syncerSounds.flag[ file ] != 1 )
+				{
+					// 生成するエレメントの調整
+					var audio = document.createElement( 'audio' ) ;
+	
+					// エレメントのIDを指定
+					audio.id = file ;
+	
+					
+	
+					// ブラウザが[.wav]に対応している場合は[.wav]を読み込む
+					if( audio.canPlayType( 'audio/wav' ) )
+					{
+						audio.src = setDir + file + '.wav' ;
+					}
+	
+					// [audio]を生成する
+					document.body.appendChild( audio ) ;
+				}
+	
+				// 初回再生以外だったら音声ファイルを巻き戻す
+				stopCurrentSound() ;
+	
+				// 音声ファイルを再生[play()]する
+				document.getElementById( file ).play() ;
+	
+				if(file ==="kan"){document.area1.src = "images/ue.jpg"}
+				if(file ==="ice"){document.area1.src = "images/sita.jpg"}
+				if(file ==="soda"){document.area1.src = "images/migi.jpg"}
+				if(file ==="wave"){document.area1.src = "images/ue.jpg"}
+				if(file ==="fastener"){document.area1.src = "images/sita.jpg"}
+				if(file ==="pet"){document.area1.src = "images/hidari.jpg"}
+				if(file ==="spray"){document.area1.src = "images/ue.jpg"}
+				if(file ==="tomato"){document.area1.src = "images/sita.jpg"}
+				if(file ==="rain"){document.area1.src = "images/migi.jpg"}
+				if(file ==="huurin"){document.area1.src = "images/migi.jpg"}
+				if(file ==="suiteki"){document.area1.src = "images/ue.jpg"}
+				if(file ==="iki"){document.area1.src = "images/migi.jpg"}
+				if(file ==="potato"){document.area1.src = "images/sita.jpg"}
+				if(file ==="egg"){document.area1.src = "images/hidari.jpg"}
+				if(file ==="patipati"){document.area1.src = "images/migi.jpg"}
+				if(file ==="men"){document.area1.src = "images/hidari.jpg"}
+			
+				// 最近再生した音声としてセット
+				syncerSounds.currentTime = file ;
+	
+				
+				// 終了
+				return false ;
+			}
+		}
+
 	// 前回の音声を停止する関数
 	function stopCurrentSound()
 	{
@@ -99,4 +314,3 @@ var syncerSounds = {
 	
 
 })() ;
-
